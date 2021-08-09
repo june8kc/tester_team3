b697db5c7dc22914dd59ff7b56c0e6aefc54e8b8
Ugur <ugur-n@yandex.com>
2021-08-09 14:58:08 -0400

    third comment

--- src/com/mod/Main.java
+++ src/com/mod/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("first modification");
         System.out.println("second modification");
+        System.out.println("third modification");
     }
 }
