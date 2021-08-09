6addeb0a690d02e0b140b921d46ecbd3ce7d39f6
Ugur <ugur-n@yandex.com>
2021-08-09 14:24:27 -0400

    only main committed

--- src/com/mod/Main.java
+++ src/com/mod/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("first modification");
+        System.out.println("second modification");
     }
 }
