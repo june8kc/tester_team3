f683d9aff553000be1e580d18fcaf8d2f57ba3c2
Altay666 <mahmutaltay66@hotmail.com>
2021-08-11 01:09:10 +0600

    second commit

--- src/com/mod/Altay.java
+++ src/com/mod/Altay.java
@@ -3,5 +3,6 @@ package com.mod;
 public class Altay {
     public static void main(String[] args) {
         System.out.println("Altay was here...");
+        System.out.println("deneme 2");
     }
 }
